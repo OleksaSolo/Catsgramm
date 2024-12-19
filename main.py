5a70df21da5c06aa116f9ec6e38f9bcd68e02c8b
OleksaSolo <131910119+OleksaSolo@users.noreply.github.com>
2024-12-19 10:34:29 +0100

    Update main.py
    New Front

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi_limiter import FastAPILimiter
 from fastapi.middleware.cors import CORSMiddleware
 from scr.routes import notes, users, tags, auth, cloud
 from scr.conf.config import config
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -29,6 +30,14 @@ async def startup():
                           password=config.REDIS_PASSWORD, decode_responses=True)
     await FastAPILimiter.init(r)
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Hello World! We are present GatsGramm!!!"}
+    return """
+    <html>
+        <body>
+            <h1>Hello world, Team09 Python16 GoIT present Coursework on FastAPI</h1>
+            <a href="/docs">API Documentation</a>
+        </body>
+    </html>
+    """
+
